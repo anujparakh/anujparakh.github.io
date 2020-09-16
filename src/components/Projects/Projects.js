d5cc65f9d6d670f84bb9e5f80c5da72f01e9cfa0
Anuj Parakh <anuj@anujinfotech.com>
2020-09-16 14:03:18 -0500

    added triones-bulbs-controller project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,8 +3,9 @@ import ProjectView from '../ProjectView/ProjectView'
 import './Projects.scss'
 import mitsPlaceholder from '../../img/Mits.png'
 import mitsVideo from '../../img/MitsVid.mp4'
+import trionesLogo from '../../img/bulbcontrollerLogo.png'
 import phantomGuitarImage from '../../img/lbb.png'
-import packadvisorImage from '../../img/packadvisorLogo.png'
+import packadvisorImage from '../../img/packadvisor.png'
 import speakerImage from '../../img/threeStageSpeaker.png'
 import blendingImage from '../../img/blendingImage.jpg'
 import ultimateTTTImage from '../../img/ultimateTTT.png'
@@ -13,6 +14,8 @@ const mitsDescription = "MITS is a pair of gloves that can be used to create dif
                         "<br /><br />The glove has flex sensors and a force sensor to implement all the features of the glove. There are several modes currently: Strings, Piano and Drums.";
 const packadvisorDescription = "Packadvisor is a web application created to help people pack for trips. The app can help a user pack by providing information about the destination and the trip including weather, timezone change, currency conversions, translations etc. The user can also create packing lists and login using Google to save information." +
                                "<br /><br />I was the lead frontend developer on the four person team that created this application. The app uses Node.js and MongoDB for the backend and utilizes several REST APIs to get details about a user's trips.";
+const trionesBulbControllerDescription = "This iOS application provides an iOS widget and Siri shortcuts to control triones based smart bulbs. The current app is buggy and does not provide siri support. So, I created an iOS application to do that." +
+                                         "<br /><br />I created a Node.js server using the noble library to connect to the bulbs and send commands to them. This server waits for POST requests with the command and the iOS widget and shortcuts send requests to this server."
 const phantomGuitarDescription = "The Phantom Guitar is the perfect mix of two of my childhood hobbies: playing guitar, and arduinos/embedded development. I used the LightBlue Bean, which is a bluetooth Arduino-style device with an acceloremeter." +
                                  "<br /><br />I wrote Android and MacOS apps where the user can select guitar chords to play, and then use the Bean like a guitar pick, strumming in air while getting the feeling and sound of playing a guitar. The idea was to recreate playing Air Guitar."
 const imageBlendingDescription = "Image blending is the process of blending a source onto a target image with a mask such that it appears that the source is blending nicely into the target image's background. It can be implemented in a variety of ways. I implemented Pyramid Blending and Poisson Blending." +
@@ -79,6 +82,14 @@ export default class Projects extends React.Component {
                 demoLink="https://www.youtube.com/watch?v=v25CMmxGf4E&feature=youtu.be"
                 demoText=" to see a demo of the amplifier."
                 ></ProjectView> */}
+
+                <ProjectView title="Trions Bulb Controller"
+                description={trionesBulbControllerDescription}
+                githubLink="https://github.com/anujparakh/triones-bulbs-controller"
+                placeholderImage={trionesLogo}
+                date="September 2020"
+                mobileWidth={this.props.mobileWidth}>
+                </ProjectView>
 
                 <ProjectView title="Image Blending"
                 description={imageBlendingDescription}
